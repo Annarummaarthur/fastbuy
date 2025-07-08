7b2a75e0c10acb138d5544242f3a026579cbbb4a
Arthur <48590429+Annarummaarthur@users.noreply.github.com>
2025-07-08 14:46:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true, // rend `describe`, `it`, `expect` globaux
-    environment: 'jsdom', // simule le DOM (utile pour React)
-    setupFiles: './src/setupTests.ts', // ton fichier de configuration des tests
-    include: ['src/**/*.test.{ts,tsx}'], // où chercher les fichiers de test
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.test.{ts,tsx}'],
   },
 });
